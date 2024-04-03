6f567c7fb59d5d98c9f59e211712bf483c1adddc
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2024-04-03 16:38:16 +0530

    Add files via upload

---
+++ ADS/Day2/Recursion7.java
@@ -0,0 +1,22 @@
+public class Recursion7{
+	
+	static void toh(int n,char s, char inter, char d)
+	{
+		if(n==1)
+			System.out.println(s+" to "+d);
+		else
+		{
+			toh(n-1,s,d,inter);
+			System.out.println(s+" to "+d);
+			toh(n-1,inter,s,d);
+		}
+		
+	}
+	
+	
+	public static void main(String args[])
+	{
+		toh(2,'A','B','C');
+		
+	}
+}
