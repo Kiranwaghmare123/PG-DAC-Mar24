165a52ce3bb28138174bbee39addb13a9aaf708f
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2024-04-04 15:11:25 +0530

    Update List1.java

--- ADS/Day3/List1.java
+++ ADS/Day3/List1.java
@@ -1,1 +1,40 @@
-
+//List creation and travers the list
+class List1{
+	
+	Node head;//instance
+	static class Node{
+		
+		int data;
+		Node next;
+		
+		Node(int d)
+		{
+			data=d;
+			next=null;
+		}
+	}
+	
+	void display()
+{
+	Node n = head;
+	while(n!= null)
+	{
+		System.out.print(n.data+ "---> ");
+		n=n.next;
+	}
+}
+	
+	public static void main(String args[]){
+		
+		Linkedlist1 L1 = new Linkedlist1();
+		L1.head = new Node(5);
+		Node second = new Node(7);
+		Node third = new Node(9);
+		
+		L1.head.next = second;
+		second.next = third;
+		
+		L1.display();
+	
+	}
+}
